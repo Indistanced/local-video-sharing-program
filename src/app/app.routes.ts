539b9909aaa8bacdc1e429c92bc9ce486fdb9dd8
Indistanced <aidank1045@gmail.com>
2024-07-18 14:32:19 +0100

    Implement routing to navigation bar

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { BrowseComponent } from './browse/browse.component';
+import { FaqComponent } from './faq/faq.component';
+import { ContactComponent } from './contact/contact.component';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+    { path: '', component: HomeComponent },
+    { path: 'browse', component: BrowseComponent },
+    { path: 'faq', component: FaqComponent },
+    { path: 'contact', component: ContactComponent }
+];
